ed91e8f5632d617633c6fb1925e8c72fd564d542
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2013-10-03 11:44:38 +0300

    cleanup

--- grails-app/routes/TestRoutes.groovy
+++ grails-app/routes/TestRoutes.groovy
@@ -1,15 +1,9 @@
 import org.apache.camel.builder.RouteBuilder
-import org.apache.camel.LoggingLevel
 
 class TestRoutes extends RouteBuilder {
-
-  def grailsApplication
-
   @Override 
   void configure(){
     from("direct:foo").
-      routeId('testRoute').
-      log(LoggingLevel.WARN, "org.apache.camel", '${body}').
       to("mock:bar")
   }
 }
